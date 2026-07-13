90b39046f6d34a3f4246d9f8104e63de29af36cb
Ruby-Kiener <rubydsl@gmx.de>
2026-07-13 22:55:16 +0200

    korrigierte gui

---
+++ src/WaehrungsrechnerGui.java
@@ -0,0 +1,28 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class WaehrungsrechnerGui {
+    private JLabel lb;
+    private JButton btnUmrechnen;
+    private JButton btnLöschen;
+    private JTextField tfUSDollar;
+    private JTextField tfEuro;
+    private JTextField tfRupiah;
+    private JPanel panel;
+
+    public WaehrungsrechnerGui() {
+        btnUmrechnen.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        btnLöschen.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
+}
